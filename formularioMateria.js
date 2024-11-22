b7b2adf5ce419ac9cd8530fda9b3f7ab7f918015
LarissaKuster <160235542+LarissaKuster@users.noreply.github.com>
2024-11-22 09:21:51 -0300

    add index e add a url

--- formularioMateria.js
+++ formularioMateria.js
@@ -1,5 +1,5 @@
-const URL_API_MATERIA = 'http://localhost:3000/materia/'
-const URL_API_PROFESSOR = 'http://localhost:3000/professor/'
+const URL_API_MATERIA = 'https://backend-render-y3ud.onrender.com/materia/'
+const URL_API_PROFESSOR = 'http://https://backend-render-y3ud.onrender.com:3000/professor/'
 const URL_REDICIONAMENTO = './materias.html'
 
 const urlParams = new URLSearchParams(window.location.search);
